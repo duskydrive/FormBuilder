1962fffa4cdb0455c9c9afdf206822c63071456d
furrysweet-bigmuff <greyhotel2@gmail.com>
2023-03-06 13:12:54 +0100

    fix(style): migrated from scss to sass

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.sass']
 })
 export class LoginComponent {
 
