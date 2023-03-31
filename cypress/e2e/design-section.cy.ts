4bffeba5bc9fe214533c75900c9bbe1ac61e7389
furrysweet-bigmuff <greyhotel2@gmail.com>
2023-03-31 14:37:30 +0200

    Tests(dropsection): add more drop section tests

--- cypress/e2e/design-section.cy.ts
+++ cypress/e2e/design-section.cy.ts
@@ -5,21 +5,35 @@ describe('Design Section', () => {
     cy.login(userCreds);
     cy.get('#accordion-header-1')
       .invoke('attr', 'aria-expanded')
-      .should('eq', 'true')
+      .should('eq', 'true');
   })
 
   it('Should have "Field Styling" closed by default', () => {
     cy.login(userCreds);
     cy.get('#accordion-header-2')
       .invoke('attr', 'aria-expanded')
-      .should('eq', 'false')
+      .should('eq', 'false');
   })
 
   it('Should change heading in DropSection when user types in Form Heading Input', () => {
     cy.login(userCreds);
-    // cy.get('#generalDesignHeading').clear().type('New heading')
-    // cy.get('#dropHeading').should('have.text', 'New heading');
-    cy.get('#dropHeading').contains('Form Builder1');
-    // .should('have.text', 'Form Builder');
+    cy.get('#generalDesignHeading').clear().type('New heading');
+    cy.get('#dropHeading').should('contain.text', 'New heading');
+  })
+
+  it('Should change border-width of form in DropSection when user types in Form Border Width Input', () => {
+    cy.login(userCreds);
+    cy.get('#generalDesignBorderWidth').clear().type('12');
+    cy.get('#dropForm').should('have.css', 'border-width', '12px');
+  })
+
+  it('Should change border-color of form in DropSection when user types in Form Border Color Input', () => {
+    cy.login(userCreds);
+    cy.get('#generalDesignBorderColor')
+      .click()
+      .invoke('val', '#ff0000')
+      .trigger('input')
+      .blur()
+    cy.get('#dropForm').should('have.css', 'border-color', '#ff0000');
   })
 })
