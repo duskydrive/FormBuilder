97c74d2cc0cf6dd7552248752d47290f200c11ce
furrysweet-bigmuff <greyhotel2@gmail.com>
2023-04-07 12:58:21 +0200

    minor fix

---
+++ src/app/shared/ts/form-label.enum.ts
@@ -0,0 +1,13 @@
+export enum FormLabel {
+  placeholderText = 'Placeholder text',
+  width = 'Width',
+  height = 'Height',
+  required = 'Required',
+  borderWidth = 'Border width',
+  borderColor = 'Border color',
+  borderStyle = 'Border style',
+  fontSize = 'Font size',
+  fontWeight = 'Font weight',
+  color = 'Color',
+  backgroundColor = 'Background color',
+}
